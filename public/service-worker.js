a00340068fdd9def919231572ab78f3a8baad4d5
Carlos Castillo <castillo25ca@gmail.com>
2021-04-22 08:56:02 -0700

    updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,15 @@
 const FILES_TO_CACHE = [
-    "/", "/index.html","index.js", "/db.js", "/style.css"];
+  "/",
+  "/index.html",
+  "index.js",
+  "/db.js",
+  "/style.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log("Your files were pre-cached successfully!");
@@ -17,7 +21,7 @@ self.addEventListener("install", function(evt) {
 });
 
 // activate
-self.addEventListener("activate", function(evt) {
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
@@ -36,28 +40,28 @@ self.addEventListener("activate", function(evt) {
 
 // fetch
 self.addEventListener("fetch", evt => {
-    if(evt.request.url.includes('/api/')) {
-        console.log('[Service Worker] Fetch(data)', evt.request.url);
-    
-evt.respondWith(
-                caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                .then(response => {
-                    if (response.status === 200){
-                        cache.put(evt.request.url, response.clone());
-                    }
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-            })
-            );
-            return;
-        }
+  if (evt.request.url.includes('/api/')) {
+    console.log('[Service Worker] Fetch(data)', evt.request.url);
 
-evt.respondWith(
-    caches.open(CACHE_NAME).then( cache => {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
+          });
+      })
+    );
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
       return cache.match(evt.request).then(response => {
         return response || fetch(evt.request);
       });
