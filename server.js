0104f8365cc14ea757cbbc14e2cf589ed4e8bd9f
Carlos Castillo <castillo25ca@gmail.com>
2021-04-22 09:16:42 -0700

    update to server.js

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-// Dependencies
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
-// Setting up Express App
+const PORT = 3000;
+
 const app = express();
-const PORT = process.env.PORT || 8000;
 
-app.use(morgan("dev"));
+app.use(logger("dev"));
 
-// Sets up the Express app to handle data parsing
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-// db mongo
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget-Tracker-App", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useFindAndModify: false
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
-// Creating Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// routes
+app.use(require("./routes/api.js"));
 
-// Starts the server to begin listening
-app.listen(PORT, function () {
-    console.log(`App listening on Port ${PORT}!`);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
